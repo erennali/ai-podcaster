3e5850de196bbcb551f3a30b18651e7ac0d0ece7
Eren Ali Koca <eren_ali_koca@hotmail.com>
2025-04-27 14:37:17 +0300

    feat(tabBar):created TabBar

---
+++ AI Podcaster/TabBar/TabBarController.swift
@@ -0,0 +1,70 @@
+//
+//  TabBarController.swift
+//  AI Podcaster
+//
+//  Created by Eren Ali Koca on 27.04.2025.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    //MARK: Properties
+
+    //MARK: Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTabs()
+    }
+    
+}
+
+    //MARK: - Private Methods
+
+private extension TabBarController {
+    //CTRL-M
+    func setupTabs () {
+        let homeVC = createNav(
+            with: "Home",
+            and: UIImage(systemName: "house.fill"),
+            viewController: PodcastsViewController()
+        )
+        let searchVC = createNav(
+            with: "Search",
+            and: UIImage(systemName: "magnifyingglass"),
+            viewController: SearchViewController()
+        )
+        let createrPodcast = createNav(
+            with: "Create Podcast",
+            and: UIImage(systemName: "plus"),
+            viewController: CreaterPodcastsViewController()
+        )
+        let libraryVC = createNav(
+            with: "Library",
+            and: UIImage(systemName: "music.note"),
+            viewController: LibraryViewController()
+        )
+        let profileVC = createNav(
+            with: "Settings",
+            and: UIImage(systemName: "gear"),
+            viewController: ProfileViewController()
+        )
+        setViewControllers([homeVC,searchVC, createrPodcast,libraryVC, profileVC], animated: false)
+    }
+    
+    func createNav(
+        with title : String,
+        and image : UIImage?,
+        viewController : UIViewController
+    ) -> UINavigationController {
+        let controller = UINavigationController(rootViewController: viewController)
+        controller.tabBarItem.title = title
+        controller.tabBarItem.image = image
+        viewController.title = title
+        controller.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.largeTitleDisplayMode = .always
+        return controller
+    }
+}
