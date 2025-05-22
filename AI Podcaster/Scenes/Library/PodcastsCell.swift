c27a98cebfa4d59d3af80b0e31f32826e01ded7a
erennali <eren_ali_koca@hotmail.com>
2025-05-22 22:48:39 +0300

    feat: podcast cell podcast label rengi değiştirildi

--- AI Podcaster/Scenes/Library/PodcastsCell.swift
+++ AI Podcaster/Scenes/Library/PodcastsCell.swift
@@ -52,7 +52,7 @@ class PodcastsCell: UITableViewCell {
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        label.textColor = UIColor(red: 0.3, green: 0.3, blue: 0.8, alpha: 1.0)
+        label.textColor = .label
         label.numberOfLines = 2
         return label
     }()
