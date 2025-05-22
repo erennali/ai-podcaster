b5b7017162675f503aa6b2216ba79ee51029aa25
erennali <eren_ali_koca@hotmail.com>
2025-05-22 19:55:50 +0300

    feat: viewWillAppear ile PodcastVC reloadData işlemi eklendi

--- AI Podcaster/Scenes/Library/PodcastsViewController.swift
+++ AI Podcaster/Scenes/Library/PodcastsViewController.swift
@@ -23,6 +23,9 @@ final class PodcastsViewController: UIViewController {
         //tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(PodcastsCell.self, forCellReuseIdentifier: PodcastsCell.identifier)
         tableView.rowHeight = 180
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.backgroundColor = .clear
         return tableView
     }()
     
@@ -41,7 +44,17 @@ final class PodcastsViewController: UIViewController {
         configureView()
         viewModel.fetchPodcasts()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Her görünüme geldiğinde podcast listesini yenile
+        viewModel.fetchPodcasts()
+        
+        // Navigasyon başlığını ayarla
+        title = "My Podcasts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
 
 private extension PodcastsViewController {
@@ -55,23 +68,41 @@ private extension PodcastsViewController {
     func addViews() {
         view.addSubview(tableView)
     }
+    
     func configureLayout() {
-            tableView.snp.makeConstraints{
-                $0.edges.equalToSuperview()
-            }
+        tableView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
         }
-    
-    
-    
+    }
 }
 
 extension PodcastsViewController: PodcastsViewControllerProtocol {
     func reloadData() {
         DispatchQueue.main.async {
             self.tableView.reloadData()
+            
+            // Veri olmadığında bir mesaj göster
+            if self.viewModel.podcasts.isEmpty {
+                self.showEmptyStateMessage()
+            } else {
+                self.hideEmptyStateMessage()
+            }
         }
+    }
+    
+    private func showEmptyStateMessage() {
+        let messageLabel = UILabel()
+        messageLabel.text = "No podcasts yet.\nCreate your first podcast from the Create tab!"
+        messageLabel.textColor = .secondaryLabel
+        messageLabel.font = .systemFont(ofSize: 16)
+        messageLabel.textAlignment = .center
+        messageLabel.numberOfLines = 0
         
-        
+        tableView.backgroundView = messageLabel
+    }
+    
+    private func hideEmptyStateMessage() {
+        tableView.backgroundView = nil
     }
 }
 
@@ -81,7 +112,6 @@ extension PodcastsViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: PodcastsCell.identifier, for: indexPath) as! PodcastsCell
         cell.configure(with: viewModel.podcasts[indexPath.row])
         return cell
